2f53d1cbed16f4882ea856c3c6fe47306fed86c0
NOLAgnarwhal <antonoverby@gmail.com>
2020-09-16 14:20:05 -0400

    Initial Commit

---
+++ injured-reserve/current-IR.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+page = 'https://www.footballdb.com/transactions/injured-reserve.html?sortfld=date&sortdir=desc#'
+
+IR_table = pd.read_html(page, attrs={'class':'statistics'})
+
+IR_df = IR_table[0]
+
+IR_df.to_csv('Current_IR.csv')
+
