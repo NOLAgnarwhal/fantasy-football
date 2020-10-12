7a19760440a40f9d842c63fc5dabe4ce6afb749f
NOLAgnarwhal <antonoverby@gmail.com>
2020-10-12 12:45:41 -0400

    no dupes in flex position

--- dfs_optim.py
+++ dfs_optim.py
@@ -1,31 +1,31 @@
 import pandas as pd 
-from pulp import *
+import pulp
 from opponent_implied_ceiling import opponent_implied_ceiling
+import re
 
+### Adds unwanted players to a list. List used to delete players from DF later on. ###
 excluded_players = []
 dont_play = input('Players to exclude (click Enter when done): ')
 while not dont_play == '':
     excluded_players.append(dont_play)
     dont_play = input('Players to exclude (click Enter when done): ')
 
+### OIC DF for DST doesn't match DK names. Most names except for Washington are at the [-1] index ###
 opponent_implied_ceiling.dst_df['Player'] = opponent_implied_ceiling.dst_df['Player'].apply(lambda x: x.split()[-1])
-
-wash_name = {
-    'Team':'WAS Football Team'
-}
-
+wash_name = {'Team':'WAS Football Team'}
 opponent_implied_ceiling.dst_df = opponent_implied_ceiling.dst_df.replace({'Player':wash_name})
 
+### Concat projection DFs and prep for merge with DK players DF ###
 dfs = [opponent_implied_ceiling.qb_df, opponent_implied_ceiling.wr_df, opponent_implied_ceiling.rb_df, opponent_implied_ceiling.te_df, opponent_implied_ceiling.dst_df]
 
 for df in dfs:
-    points_df = pd.concat(dfs).fillna(0)
+    proj_df = pd.concat(dfs).fillna(0)
 
-points_df['Name'] = points_df['Player']
+proj_df['Name'] = proj_df['Player']
 ordered_cols = ['Name', 'PPR OIC']
-points_df = points_df[ordered_cols]
+proj_df = proj_df[ordered_cols]
 
-player_name_map = {
+fix_player_name = { #will need to update manually weekly#
     'Patrick Mahomes II':'Patrick Mahomes',
     'D.J. Chark Jr.':'DJ Chark Jr.',
     'D.J. Moore':'DJ Moore',
@@ -40,75 +40,45 @@ player_name_map = {
     'John Ross':'John Ross III',
 }
 
-points_df = points_df.replace({'Name':player_name_map})
+proj_df = proj_df.replace({'Name':fix_player_name})
 
-dk_players = pd.read_csv('/home/gnarwhal/fantasy_football/dfs_optimization/salaries/wk{week}DKSalaries.csv'.format(week=opponent_implied_ceiling.week))
-
-dk_players['Name'] = dk_players['Name'].apply(lambda x: x.strip())
-
-dk_players = dk_players.merge(points_df, how='left', on='Name').fillna(0)
-
-for player in excluded_players:
-    dk_players = dk_players.loc[dk_players['Name']!=player]
-#%%
-#%%
-flex_positions = ['RB', 'WR', 'TE']
-dfs = []
-for pos in flex_positions:  
-    df = dk_players.loc[dk_players['Position'] == pos]
-    dfs.append(df)
-
-flex_players_df = pd.concat(dfs)
-flex_players_df['Position'] = flex_players_df['Position'].replace(['RB','WR','TE'], 'FLEX')
-
-dk_players = pd.concat([dk_players,flex_players_df])
-
+### Import DK player salary DF to merge with projections DF ###
+dk_players = pd.read_csv('/home/gnarwhal/fantasy_football/salaries/wk{week}DKSalaries.csv'.format(week=opponent_implied_ceiling.week))
+dk_players['Name'] = dk_players['Name'].apply(lambda x: x.strip()) #DK DF has white space for some reason#
+dk_players = dk_players.merge(proj_df, how='left', on='Name').fillna(0)
 dk_players = dk_players[['Position','Name','Salary','PPR OIC']] 
 
-salaries = {}
-points = {}
+### Excludes players from list created at beginning with user input ###
+for player in excluded_players:
+    dk_players = dk_players.loc[dk_players['Name']!=player]
 
-for pos in dk_players['Position'].unique():
-    available_pos = dk_players[dk_players['Position'] == pos]
-    
-    salary = list(available_pos[['Name','Salary']].set_index('Name').to_dict().values())[0]
+### Solve Optimization problem using puLP ###
+player_data = dk_players.set_index('Name')
 
-    point = list(available_pos[['Name','PPR OIC']].set_index('Name').to_dict().values())[0]
+plist = dk_players['Name'].tolist()
+qb = dk_players[dk_players['Position']=='QB']['Name'].tolist()
+rb = dk_players[dk_players['Position']=='RB']['Name'].tolist()
+wr = dk_players[dk_players['Position']=='WR']['Name'].tolist()
+te = dk_players[dk_players['Position']=='TE']['Name'].tolist()
+dst = dk_players[dk_players['Position']=='DST']['Name'].tolist()
 
-    salaries[pos] = salary
-    points[pos] = point
-
-pos_num_available = {
-    'QB':1,
-    'RB':2,
-    'WR':3,
-    'TE':1,
-    'FLEX':1,
-    'DST':1
-}
-
-salary_cap = 50000
-
-_vars = {k: LpVariable.dict(k, v, cat='Binary') for k, v in points.items()}
-
-prob = LpProblem('DFS Optimization', LpMaximize)
-rewards = []
-costs = []
-position_constraints = []
-
-for k, v in _vars.items():
-    costs += lpSum([salaries[k][i] * _vars[k][i] for i in v])
-    rewards += lpSum([points[k][i] * _vars[k][i] for i in v])
-    prob += lpSum([_vars[k][i] for i in v]) <= pos_num_available[k]
-
-prob += lpSum(rewards)
-prob += lpSum(costs) <= salary_cap
+prob = pulp.LpProblem('DFS Optimization', pulp.LpMaximize)
+players = pulp.LpVariable.dicts('Player: ', plist, cat='Binary')
+prob += pulp.lpSum([player_data['PPR OIC'][i] * players[i] for i in plist])
+prob += pulp.lpSum([player_data['Salary'][i] * players[i] for i in plist]) <= 50000
+prob += pulp.lpSum([players[i] for i in qb]) == 1
+prob += pulp.lpSum([players[i] for i in rb]) >= 2
+prob += pulp.lpSum([players[i] for i in wr]) >= 3
+prob += pulp.lpSum([players[i] for i in te]) >= 1
+prob += pulp.lpSum([players[i] for i in dst]) == 1
+prob += pulp.lpSum([players[i] for i in plist]) == 9
 
 prob.solve()
 
+### Function prints output into terminal when script is run ###
 def summary(prob):
     div = '---------------------------------------\n'
-    print("Variables:\n")
+    print("Optimal Lineup:\n")
     score = str(prob.objective)
     constraints = [str(const) for const in prob.constraints.values()]
     for v in prob.variables():
@@ -116,12 +86,6 @@ def summary(prob):
         constraints = [const.replace(v.name, str(v.varValue)) for const in constraints]
         if v.varValue != 0:
             print(v.name, "=", v.varValue)
-    print(div)
-    print("Constraints:")
-    for constraint in constraints:
-        constraint_pretty = " + ".join(re.findall("[0-9\.]*\*1.0", constraint))
-        if constraint_pretty != "":
-            print("{} = {}".format(constraint_pretty, eval(constraint_pretty)))
     print(div)
     print("Score:")
     score_pretty = " + ".join(re.findall("[0-9\.]+\*1.0", score))
